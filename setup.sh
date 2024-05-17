55d33d851292801e8b4a4f4920ab8ee99f79ee89
= <38575855+fishy15@users.noreply.github.com>
2024-05-17 01:48:22 -0500

    Init commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# For setting up the repository on CloudLab
+#
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+
+cd ~
+
+# create workspace
+mkdir research/
+cd research/
+
+git clone https://github.com/fishy15/linux-cfs-testing --depth=1 kernel/
+
+mkdir build/
+cd build/
+cp ../kernel/myconfig .config
+
+make -C ../kernel O="$(pwd)" -j16
+
