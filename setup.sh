ef485ea0b640c08214713a3227262cbfc8abb6d7
= <38575855+fishy15@users.noreply.github.com>
2024-06-01 00:28:47 -0700

    Add Git LFS to script

--- setup.sh
+++ setup.sh
@@ -3,9 +3,15 @@
 # For setting up the repository on CloudLab
 #
 sudo apt update && sudo apt -y upgrade
-sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison qemu-system-x86
+sudo apt install -y git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison qemu-system-x86 git-lfs
 
 cd /local/
+
+# install git-lfs and download image
+cd repository/
+git lfs install
+git lfs pull
+cd ..
 
 # create workspace
 mkdir research/
@@ -21,5 +27,3 @@ cp ../kernel/myconfig .config
 cp /local/repository/build-kernel /local/research/
 cp /local/repository/runvm /local/research/
 cp /local/repository/lfs /local/research
-
-
