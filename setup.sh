0493dbb7286ed0ac6c13a4555eca4384b45e743c
= <38575855+fishy15@users.noreply.github.com>
2024-07-29 18:21:41 -0700

    Build qemu in profile

--- setup.sh
+++ setup.sh
@@ -24,6 +24,17 @@ cd build/
 cp ../kernel/myconfig .config
 
 # move config files
-cp /local/repository/build-kernel /local/research/
-cp /local/repository/runvm /local/research/
+cp -r /local/repository/bin /local/research
 cp /local/repository/lfs /local/research
+
+# set up qemu
+sudo apt install meson ninja-build libglib2.0-dev libslirp-dev flex bison python3-pip python3-venv
+pip install tomli
+cd /local/research
+mkdir -p qemu/build
+cd qemu/build
+../configure --enable-slirp && make -j`nproc`
+ln -s /local/research/qemu/build/qemu-system-x86_64 /local/research/bin/qxd
+
+# add bin to PATH
+echo 'export PATH="$PATH:/local/research/bin"' >> ~/.bashrc
