1b89a320651071a7206bf92250df05b714dee1d2
= <38575855+fishy15@users.noreply.github.com>
2024-07-30 16:06:12 -0700

    Log output to a file

--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ request = pc.makeRequestRSpec()
 node = request.RawPC("node")
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh"))
+node.addService(pg.Execute(shell="bash", command="/local/repository/setup.sh &> /local/repository/log"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
