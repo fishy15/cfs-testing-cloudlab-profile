bd1c7d1a1d2d74afa88296300c48e00158d34df7
aprakas <aprakas@node.big-machine.wisr-pg0.utah.cloudlab.us>
2024-12-22 01:49:48 +0000

    Update run-test-as-user.sh

--- run-test-as-user.sh
+++ run-test-as-user.sh
@@ -13,8 +13,10 @@ GDB=$(bc <<<"2222 + $USER_NUMBER")
 SSH=$(bc <<<"3333 + $USER_NUMBER")
 
 f=$(mktemp)
-TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/runvm" d.q | tee $f &
-grep "localhost login:" $f | tail -n1
+rm -f $f
+TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/runvm" d.q >$f &
+#grep "localhost login:" $f | tail -n1
+grep "Debian GNU/Linux 12 localhost ttyS0" $f | tail -n1
 
 # swk will store out.json under $HOME
-ITERS=32 TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/swk" out.json "$TASK"
+ITERS=1 TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/swk" out.json "$TASK"
