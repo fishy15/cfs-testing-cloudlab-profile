9cf2bcac6acd177b8d42b4ff52d2d40c9a3747d3
aprakas <aprakas@node.big-machine.wisr-pg0.utah.cloudlab.us>
2024-12-22 01:39:29 +0000

    Update run-test-as-user.sh to work

--- run-test-as-user.sh
+++ run-test-as-user.sh
@@ -12,6 +12,9 @@ SCRIPTS="$HOME/rsch/kernel/logging-scripts"
 GDB=$(bc <<<"2222 + $USER_NUMBER")
 SSH=$(bc <<<"3333 + $USER_NUMBER")
 
-TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/runvm" lfs &
-sleep 15
-ITERS=1 TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/swk" ~/out.json "$TASK"
+f=$(mktemp)
+TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/runvm" d.q | tee $f &
+grep "localhost login:" $f | tail -n1
+
+# swk will store out.json under $HOME
+ITERS=32 TOPOLOGY=$TOPOLOGY GDB=$GDB SSH=$SSH "$SCRIPTS/swk" out.json "$TASK"
